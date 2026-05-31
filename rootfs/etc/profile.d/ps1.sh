c66e1cabf83cc9358de7a51af853a443aa269ad4
lnkrr <lnkrr@proton.me>
2026-05-31 17:00:31 +0300

    feat: improve PS1

--- rootfs/etc/profile.d/ps1.sh
+++ rootfs/etc/profile.d/ps1.sh
@@ -1,1 +1,15 @@
-export PS1='\u@\h:\w\$ '
+if [ -n "$BASH_VERSION" ]; then
+    if [ "$(id -u)" = "0" ]; then
+        PS1='\[\e[1;31m\]\u@\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]# '
+    else
+        PS1='\[\e[1;35m\]\u@\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\]\$ '
+    fi
+else
+    if [ "$(id -u)" = "0" ]; then
+        PS1='$(id -un)@$(hostname):$(pwd)# '
+    else
+        PS1='$(id -un)@$(hostname):$(pwd)\$ '
+    fi
+fi
+
+export PS1
